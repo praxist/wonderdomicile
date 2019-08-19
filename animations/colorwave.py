17b50ebcde416aff92fcb5447b650b00cc4d4c13
Matt Leung <leung.matt.p@gmail.com>
2019-08-19 16:07:22 -0700

    Add blooming colorwave

--- animations/colorwave.py
+++ animations/colorwave.py
@@ -16,14 +16,33 @@ class Horizontal(Matrix):
         self._step += amt
 
 class Vertical(Matrix):
-    def __init__(self, *args, **kwds):
+    def __init__(self, *args,
+                 bloom=False,
+                 color_speed=2,
+                 color_distance=2,
+                 **kwds):
         #The base class MUST be initialized by calling super like this
+
+        # Causes the color to bloom from the center
+        self.bloom = bloom
+
+        # higher values make colors change faster
+        self.color_speed = color_speed
+
+        # higher values display more colors in a single frame
+        self.color_distance = color_distance
+
         super().__init__(*args, **kwds)
 
     def step(self, amt=1):
-        for i in range(self.layout.width):
-            color = self.palette(1*i + self._step)
-            for j in range(self.layout.height):
+        for j in range(self.layout.height):
+            if self.bloom:
+                distance = abs(self.layout.height / 2 - j)
+                color = self.palette(self._step * self.color_speed - distance * self.color_distance)
+            else:
+                color = self.palette(self.color_speed * j + self._step * self.color_distance)
+
+            for i in range(self.layout.width):
                 self.layout.set(i,j,color)
 
         self._step += amt
